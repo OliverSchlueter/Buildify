c1f1769a65edad60329ffac1815f2ae5439e777c
OliverSchlueter <oliverhd.1303@gmail.com>
2023-04-04 14:40:27 +0200

    [web] add temp History

--- buildify-web/src/App.vue
+++ buildify-web/src/App.vue
@@ -1,17 +1,18 @@
 <script lang="ts">
+import History from "./components/History.vue";
+
 export default {
-  data() {
-    return {
-      
+    components: {
+      History
     }
-  }
 }
 </script>
 
 <template>
-  
+  <h1>Buildify builds</h1>
+  <History link="localhost:1337/"/>
 </template>
 
 <style>
-
+  
 </style>
---
+++ buildify-web/src/components/History.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+export default {
+  props: [
+    "link"
+  ],
+  computed:{
+    builds(){
+      fetch(this.link + "builds").then(res => {
+        console.log(res);
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> link: {{ link }}</h1>
+</template>
+
+<style>
+  
+</style>
